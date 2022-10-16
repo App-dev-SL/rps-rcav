e0ad4948a67604a372413772ca4330382b06d789
Summer-Lu0208 <slu12@chicagobooth.edu>
2022-10-16 04:02:18 +0000

    lots of progress on paper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,28 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
- def play_rock
+
+  def homepage
+    render({:template=>"game_templates/rules.html.erb"})
+  end
+
+  def play_rock
 
    #redirect_to("https://www.wikipedia.org")
    #render({:html=>"<h1>Hello,world!</h1>".html_safe})
    render({:template=>"game_templates/user_rock.html.erb"})
 
- end
+end
+
+   def play_paper
+
+  render({:template=>"game_templates/user_paper.html.erb"})
+
+end
+
+def play_scissors
+
+  render({:template=>"game_templates/user_scissors.html.erb"})
+
+end
 end
